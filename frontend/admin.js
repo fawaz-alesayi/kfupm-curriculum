92bd681158dbc4509f6c250d29ba43e65323f905
Khloox <63790011+Khloox@users.noreply.github.com>
2020-07-11 23:51:16 +0300

    adding Front End Files

---
+++ frontend/admin.js
@@ -0,0 +1,85 @@
+
+var up = document.getElementById('GFG_UP'); 
+var down = document.getElementById('GFG_DOWN'); 
+var div = document.getElementById('cox'); 
+              
+function GFG_Function() { 
+    div.parentNode.removeChild(div); 
+    down.innerHTML = "Element is removed.";  
+    } 
+
+    function GFS_Function() { 
+        div.parentNode.add(div,div); 
+        down.innerHTML = "Element is added.";  
+        } 
+
+    $('select').change(function(){
+          if($('select option:selected').text() == "Course Description"){
+             $('label').show();
+          }
+          else{
+             $('label').hide();
+          }
+      });
+
+    function addTextArea1(){
+        var div = document.getElementById('div_quotes1');
+
+        div.innerHTML += "<textarea name='new_quote1[]' />";
+        div.innerHTML += "\n<br />";
+
+    }
+
+    // function addUploadArea2(){
+    //     var div = document.getElementById('div_quotes2');
+
+    //     div.innerHTML += "<file name='new_quote2[]' />";
+    //     div.innerHTML += "\n<br />";
+
+    // }
+
+
+    function addTextArea3(){
+        var div = document.getElementById('div_quotes3');
+
+        div.innerHTML += "<textarea name='new_quote3[]' />";
+        div.innerHTML += "\n<br />";
+
+    }
+
+
+    function addTextArea4(){
+        var div = document.getElementById('div_quotes4');
+
+        div.innerHTML += "<textarea name='new_quote4[]' />";
+        div.innerHTML += "\n<br />";
+
+    }
+
+    function addTextArea5(){
+        var div = document.getElementById('div_quotes5');
+
+        div.innerHTML += "<textarea name='new_quote5[]' />";
+        div.innerHTML += "\n<br />";
+
+    }
+
+    function addTextArea6(){
+        var div = document.getElementById('div_quotes6');
+
+        div.innerHTML += "<textarea name='new_quote6[]' />";
+        div.innerHTML += "\n<br> You have to add URL for the New Source !";
+        div.innerHTML += "\n<br />";
+
+    }
+
+    function setup() {
+        document.getElementById('cco').addEventListener('click', openDialog);
+        function openDialog() {
+            document.getElementById('fileid').click();
+        }
+        document.getElementById('fileid').addEventListener('change', submitForm);
+        function submitForm() {
+            document.getElementById('formid').submit();
+        }
+    }
---
+++ frontend/index.js
@@ -0,0 +1,151 @@
+// Changing Color of the text when mouse on the text, FROM HERE
+function changeColor(obj) {
+    if (obj.style.color == 'indigo') {
+        obj.style.color = 'white';
+    } else {
+        obj.style.color = 'indigo';
+    }
+}
+
+function changeColorM(obj) {
+    if (obj.style.color == 'indigo') {
+        obj.style.color = 'black';
+    } else {
+        obj.style.color = 'indigo';
+    }
+}
+
+function changeColorH(obj) {
+    if (obj.style.color == 'cyan') {
+        obj.style.color = 'white';
+    } else {
+        obj.style.color = 'cyan';
+    }
+}
+
+function changeColorL(obj) {
+    if (obj.style.color == 'teal') {
+        obj.style.color = 'white';
+    } else {
+        obj.style.color = 'teal';
+    }
+}
+
+function changeColorLM(obj) {
+    if (obj.style.color == 'teal') {
+        obj.style.color = 'purple';
+    } else {
+        obj.style.color = 'teal';
+    }
+}
+// TO HERE
+
+//Header will contain only KFUPM Curriculum and hamgurger icon for remaining
+function toggleNav() {
+    let nav = document.getElementsByClassName("dropDownNav")[0]
+    console.assert(nav != null)
+    if (nav.classList.contains("toggled"))
+        nav.classList.remove('toggled')
+    else
+        nav.classList.add("toggled")
+}
+
+//Navigation Being sticky on scrolling
+window.onscroll = function() {scrolls()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function scrolls() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+//Flipping hamburger icon to close icon
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+// SILDE SHOW IN THE HOME PAGE !!!
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function plusSlides(n){
+  clearInterval(myTimer);
+  if (n < 0){
+    showSlides(slideIndex -= 1);
+  } else {
+   showSlides(slideIndex += 1); 
+  }
+  if (n === -1){
+    myTimer = setInterval(function(){plusSlides(n + 2)}, 6000);
+  } else {
+    myTimer = setInterval(function(){plusSlides(n + 1)}, 6000);
+  }
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+
+
+  for (i = 0; i < dots.length; i++) { 
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+
+}
+
+window.addEventListener("load",function() {
+  showSlides(slideIndex);
+  myTimer = setInterval(function(){plusSlides(1)}, 6000);
+})
+
+function currentSlide(n){
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(n + 1)}, 6000);
+  showSlides(slideIndex = n);
+}
+
+var slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+slideshowContainer.addEventListener('mouseenter', pause)
+slideshowContainer.addEventListener('mouseleave', resume)
+
+pause = () => {
+  clearInterval(myTimer);
+}
+
+resume = () =>{
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(slideIndex)}, 6000);
+}
+// END OF SILDE SHOW IN THE HOME PAGE !!!
+
